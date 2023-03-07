6cc2b3eb2036dc457e5147ae7f8fa246d5249847
mozambiquepaul <mozambiquepaul@gmail.com>
2023-03-07 18:49:04 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,25 +1,51 @@
+import os
 import tweepy
+import openai
 
-# Replace the placeholders with your own values
-consumer_key = 'CONSUMER_KEY'
-consumer_secret = 'CONSUMER_SECRET'
-access_token = 'ACCESS_TOKEN'
-access_token_secret = 'ACCESS_TOKEN_SECRET'
-client_id = 'OAUTH2_CLIENT_ID'
-client_secret = 'OAUTH2_CLIENT_SECRET'
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-auth.apply_auth()
-
-# Add OAuth 2.0 client credentials to auth object
-auth.set_client(client_id, client_secret)
-
+# Authenticate with Twitter API
+auth = tweepy.OAuth2BearerHandler(os.environ['BEARER_TOKEN'])
 api = tweepy.API(auth)
 
-# Test authentication
-try:
-    user = api.verify_credentials()
-    print('Authentication successful')
-except tweepy.TweepError as e:
-    print(f'Error: {e}')
+# Authenticate with OpenAI API
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+# Generate clown response
+def generate_clown_response(message):
+    prompt = f"Clown-themed response to: {message}"
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        timeout=10,
+    )
+    return response.choices[0].text.strip()
+
+# Stream listener to respond to DMs and mentions
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        if status.author.id == api.me().id:
+            return
+        if status.in_reply_to_status_id is not None or 'media' in status.entities:
+            return
+        if status.in_reply_to_screen_name == api.me().screen_name:
+            response = generate_clown_response(status.text)
+            api.update_status(
+                status=response,
+                in_reply_to_status_id=status.id,
+                auto_populate_reply_metadata=True
+            )
+        elif hasattr(status, 'direct_message'):
+            message = status.direct_message['text']
+            response = generate_clown_response(message)
+            api.send_direct_message(
+                user_id=status.direct_message['sender_id'],
+                text=response
+            )
+
+# Start streaming
+stream_listener = MyStreamListener()
+stream = tweepy.Stream(auth=api.auth, listener=stream_listener)
+stream.filter(track=[api.me().screen_name])
