43a80fe04a05f464859081d98eed37571c3f0f93
mozambiquepaul <mozambiquepaul@gmail.com>
2023-03-07 20:31:28 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,61 +1,79 @@
-import tweepy
 import os
+import tweepy
 import openai
 
-# set up Twitter API credentials
-consumer_key = os.getenv("TWITTER_CONSUMER_KEY")
-consumer_secret = os.getenv("TWITTER_CONSUMER_SECRET")
-access_token = os.getenv("TWITTER_ACCESS_TOKEN")
-access_token_secret = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+# Authenticate to Twitter
+auth = tweepy.OAuth1UserHandler(
+    os.environ["TWITTER_API_KEY"],
+    os.environ["TWITTER_API_SECRET_KEY"],
+    os.environ["TWITTER_ACCESS_TOKEN"],
+    os.environ["TWITTER_ACCESS_TOKEN_SECRET"]
+)
 
-# set up OpenAI API credentials
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# Authenticate to OpenAI
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# set up Tweepy API client
-auth = tweepy.OAuth2BearerHandler(access_token)
-api = tweepy.API(auth)
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-# set up listener for incoming DMs and mentions
-class ClownListener(tweepy.Stream):
-    def __init__(self, auth, api, access_token, access_token_secret):
-        super().__init__(auth, api)
-        self.access_token = access_token
-        self.access_token_secret = access_token_secret
+# Define function to respond to mentions
+def respond_to_mention(tweet):
+    # Get tweet text and author
+    text = tweet.text
+    author = tweet.author.screen_name
+    
+    # Ignore retweets and own tweets
+    if "RT @" in text or author == "HobbleStepN":
+        return
+    
+    # Get AI response
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=text,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    
+    # Post response as a tweet
+    api.update_status(
+        status="@" + author + " " + response.choices[0].text,
+        in_reply_to_status_id=tweet.id
+    )
 
+# Define function to respond to DMs
+def respond_to_dm(dm):
+    # Get DM text and sender
+    text = dm.message_create["message_data"]["text"]
+    sender_id = dm.message_create["sender_id"]
+    
+    # Get AI response
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=text,
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.7
+    )
+    
+    # Post response as a DM
+    api.send_direct_message(
+        recipient_id=sender_id,
+        text=response.choices[0].text
+    )
+
+# Set up stream listener to respond to mentions and DMs
+class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
-        # check if mention includes @HobbleStepN
-        if "@HobbleStepN" in status.text:
-            # generate clown-themed response using OpenAI API
-            response = openai.Completion.create(
-              engine="davinci",
-              prompt=status.text,
-              temperature=0.7,
-              max_tokens=60,
-              n=1,
-              stop=None,
-              timeout=10,
-            )
-            # post response as a tweet
-            api.update_status(f"@{status.user.screen_name} {response.choices[0].text}", status.id)
-        # check if DM received
-        elif status.direct_message:
-            # generate clown-themed response using OpenAI API
-            response = openai.Completion.create(
-              engine="davinci",
-              prompt=status.text,
-              temperature=0.7,
-              max_tokens=60,
-              n=1,
-              stop=None,
-              timeout=10,
-            )
-            # post response as a DM
-            api.send_direct_message(status.user.id, f"{response.choices[0].text}")
+        if status.in_reply_to_status_id is not None or status.text.startswith('@'):
+            respond_to_mention(status)
+    
+    def on_direct_message(self, dm):
+        respond_to_dm(dm)
 
-# set up Tweepy API client
-auth = tweepy.OAuth2BearerHandler(access_token)
-api = tweepy.API(auth)
-
-# start Tweepy stream
-stream = ClownListener(auth, api)
-stream.filter(track=["@HobbleStepN"], is_async=True)
+# Start stream listener
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+myStream.filter(track=["@HobbleStepN"])
