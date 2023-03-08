4d3f5a195938d2e8ca1078bcba0fb494db3493e7
mozambiquepaul <mozambiquepaul@gmail.com>
2023-03-07 19:34:50 -0500

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,49 +1,63 @@
+import os
 import tweepy
 import openai
-import os
+from tweepy import Stream, OAuth2BearerHandler
+from tweepy.streaming import StreamListener
 
 # Twitter API credentials
-consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY")
+CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
+ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
 # OpenAI API credentials
-openai.api_key = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # Authenticate with Twitter API
-auth = tweepy.OAuth2BearerHandler(access_token)
+auth = tweepy.OAuth2BearerHandler(CONSUMER_KEY, CONSUMER_SECRET)
 api = tweepy.API(auth)
 
-# Define a listener class for DMs and mentions
-class ClownBotListener(tweepy.Stream):
+# Authenticate with OpenAI API
+openai.api_key = OPENAI_API_KEY
 
+# Define a listener to handle incoming DMs and mentions
+class ClownStreamListener(StreamListener):
+    
     def on_direct_message(self, status):
-        print(f"Received DM from @{status.sender_screen_name}: {status.text}")
-        response = generate_clown_response(status.text)
-        send_dm(status.sender_screen_name, response)
-
+        # Generate a response to the DM using OpenAI
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=status.message_create["message_data"]["text"],
+            temperature=0.5,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            )
+        
+        # Send the generated response back to the user
+        user_id = status.message_create["sender_id"]
+        api.send_direct_message(user_id, text=response.choices[0].text)
+        
     def on_status(self, status):
-        if status.in_reply_to_screen_name == "HobbleStepN":
-            print(f"Received mention from @{status.user.screen_name}: {status.text}")
-            response = generate_clown_response(status.text)
-            api.update_status(f"@{status.user.screen_name} {response}", status.id)
+        # Check if the tweet mentions our username
+        if api.get_user(status.user.id_str).screen_name == "HobbleStepN":
+            # Generate a response to the tweet using OpenAI
+            response = openai.Completion.create(
+                engine="davinci",
+                prompt=status.text,
+                temperature=0.5,
+                max_tokens=100,
+                n=1,
+                stop=None,
+                )
+            
+            # Send the generated response back to the user
+            api.update_status(
+                status="@" + status.user.screen_name + " " + response.choices[0].text,
+                in_reply_to_status_id=status.id_str,
+            )
 
-# Define a function to generate clown responses using OpenAI's GPT-3
-def generate_clown_response(text):
-    prompt = f"ClownBot: {text}\nClown:"
-    response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=100, n=1,stop=None,temperature=0.5)
-    return response.choices[0].text.strip()
-
-# Define a function to send DMs
-def send_dm(screen_name, text):
-    try:
-        api.send_direct_message(screen_name=screen_name, text=text)
-        print(f"Sent DM to @{screen_name}: {text}")
-    except tweepy.TweepError as e:
-        print(f"Failed to send DM to @{screen_name}: {e}")
-
-# Start the listener
-listener = ClownBotListener(consumer_key, consumer_secret, access_token, access_token_secret)
-listener.filter(track=["@HobbleStepN"], threaded=True)
-
+# Create a stream to listen for incoming DMs and mentions
+stream_listener = ClownStreamListener()
+stream = Stream(auth, stream_listener)
+stream.filter(track=["@HobbleStepN"], is_async=True)
